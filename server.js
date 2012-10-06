d7a2ce61ab1350b82a7359ccde1f4ec1dcf3f343
Swaroop Kumar <swaroopkumar.badam@gmail.com>
2012-10-06 21:41:31 +0530

    init

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	var res = "<h1>Hello World!</h1><br/><b>Welcome to Swaroop's Test Website</b>";
+    response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
